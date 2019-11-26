8e37e17a552a81cd9742ff3d0ddf8e8420498b56
Jaemin Nam <jaemin.nam@hlag.com>
2019-11-26 11:49:25 +0100

    https-proxy - typo..probably

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 sed -i "s/INPORT/${INPORT}/g" /etc/nginx/conf.d/default.conf
-openssl req -x509 -nodes -days 3650 -subj "/C=DE/ST=Hamburg/O=https-proxy/CN=localhost" -addext "subjectAltName=DNS:localhost" -newkey rsa:2048 -keyout /tmp/nginx/nginx-selfsigned.key -out /tmp/nginx/nginx-selfsigned.crt;
+openssl req -x509 -nodes -days 3650 -subj "/C=DE/ST=Hamburg/O=https-proxy/CN=localhost" -addext "subjectAltName=DNS:localhost" -newkey rsa:2048 -keyout /tmp/nginx/nginx-selfsigned.key -out /tmp/nginx/nginx-selfsigned.crt
 
 # run additional commands given by Dockerfile
 exec "$@"
