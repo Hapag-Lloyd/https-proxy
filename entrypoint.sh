7ce5784973e86b7e642986f948bf1efac39a801c
kayma <kayma@hlag.com>
2020-05-14 21:39:26 +0200

    Rename INPORT to TARGET_HOST_PORT as we support hostnames/IP now, e.g. "nexus:12345"

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-sed -i "s/INPORT/${INPORT}/g" /etc/nginx/conf.d/default.conf
+sed -i "s/TARGET_HOST_PORT/${TARGET_HOST_PORT}/g" /etc/nginx/conf.d/default.conf
 openssl req -x509 -nodes -days 3650 -subj "/C=DE/ST=Hamburg/O=https-proxy/CN=localhost" -addext "subjectAltName=DNS:localhost" -newkey rsa:2048 -keyout /tmp/nginx/nginx-selfsigned.key -out /tmp/nginx/nginx-selfsigned.crt
 
 # run additional commands given by Dockerfile
